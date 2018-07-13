02f47fb2e4a7fdd4ab43c2ebea3a879f8db339fe
Eric Busboom <eric@civicknowledge.com>
2018-07-12 17:38:47 -0700

    .

--- bin/init-develop.sh
+++ bin/init-develop.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+pip install 'python-dateutil<2.7.0'
 (cd geoid && python setup.py develop)
 (cd rowgenerators && python setup.py develop)
 (cd metatab-declarations && python setup.py develop)
