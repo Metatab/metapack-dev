b33516d3da418ceebdfb163df5feaf8891e78dbb
Eric Busboom <eric@civicknowledge.com>
2020-06-24 12:35:44 -0700

    Requirements changes

---
+++ bin/install-from-directories.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+pip install -e ../rowgenerators
+pip install -e ../metatab-declarations
+pip install -e ../geoid
+
+pip install -e ../metatab
+pip install -e ../metapack
+
+pip install -e ../publicdata
+pip install -e ../metapack-build
+pip install -e ../metapack-jupyter
+pip install -e ../metapack-wp
+pip install -e ../metapack-github
