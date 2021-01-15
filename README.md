37bf44ae21ea33782d0ca58c97dec369e8690cee
Eric Busboom <eric@civicknowledge.com>
2021-01-15 14:47:51 -0800

    Added pipfile

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # metapack-dev
 Meta-repo for metatab and metapack development
 
+# Install on Mac
+
+
+    brew install gdal
+
 
 # HEAD Is Detached
 
 If you have trouble pushing of checking out, getting an error about the HEAD being detached, see this [Stack Exchange question for details.](https://stackoverflow.com/a/36375256)
-
- 
--- bin/install-from-directories.sh
+++ bin/install-from-directories.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-pip install -e ../rowgenerators
-pip install -e ../metatab-declarations
-pip install -e ../geoid
-
-pip install -e ../metatab
-pip install -e ../metapack
-
-pip install -e ../publicdata
-pip install -e ../metapack-build
-pip install -e ../metapack-jupyter
-pip install -e ../metapack-wp
-pip install -e ../metapack-github
+pipenv install --dev  -e ./rowgenerators
+pipenv install --dev  -e ./metatab-declarations
+pipenv install --dev  -e ./geoid
+pipenv install --dev  -e ./metatab
+pipenv install --dev  -e ./metapack
+pipenv install --dev  -e ./publicdata
+pipenv install --dev  -e ./metapack-build
+pipenv install --dev  -e ./metapack-jupyter
+pipenv install --dev  -e ./metapack-wp
+pipenv install --dev  -e ./metapack-github
